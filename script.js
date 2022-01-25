035f55da71c700d970336554a454ea55ebea199e
giwl-21 <muhsin.wahianwar@gmail.com>
2022-01-25 17:53:32 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function activate_button(){
+    
+}
+
+
+$(document).ready(() => {
+    $("#activate").click(()=>{
+        activate_button();
+    });
+
+});
+
+//"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg/471px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg"
