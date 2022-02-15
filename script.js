06b0b25563d4a9bcd643887d79ef2084d92bf126
giwl-21 <muhsin.wahianwar@gmail.com>
2022-02-15 17:41:13 -0500

    Updated to code completed in meeting

--- script.js
+++ script.js
@@ -1,7 +1,66 @@
-function activate_button(){
-    
+
+
+var word = "alphabet";
+console.log(word);
+for (var i in word){
+    console.log(word[i]);
 }
 
+function addArnie(){
+    var node = $("#arnie-box");
+    var newArnie = document.createElement("div");
+    newArnie.setAttribute("class", "arnie-item");
+
+    var image = document.createElement("img");
+    image.setAttribute("class", "arnie");
+    image.setAttribute("src", "arnie.jpeg");
+
+    var btn = document.createElement("button");
+    btn.innerHTML="Arnie";
+
+    newArnie.append(image);
+    newArnie.append(btn);
+    node.append(newArnie);
+}
+
+function loadArnie(){
+
+    fetch("https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg/471px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg").then((response)=>{
+        console.log("Arnie Loaded!!");
+        return response.blob();
+    }).then((myBlob)=>{
+        let objectURL = URL.createObjectURL(myBlob);
+        image = document.createElement('img');
+        image.src = objectURL;
+        document.body.appendChild(image);
+    }).then(()=>{
+        console.log("All done...");
+    }).catch((error)=>{
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+}
+
+async function generateArnieLink(){
+
+    var b = await fetch("https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg/471px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg").then((response)=>{
+        console.log("Arnie Loaded!!", response);
+        return response.blob();
+    }).then((myBlob)=>{
+        let objectURL = URL.createObjectURL(myBlob);
+        return objectURL;
+    }).catch((error)=>{
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+    return b;
+}
+
+
+function activate_button(){
+    generateArnieLink().then((result)=>{
+        console.log(result);
+        $(".arnie").attr("src", result);
+    });
+}
 
 $(document).ready(() => {
     $("#activate").click(()=>{
@@ -9,5 +68,3 @@ $(document).ready(() => {
     });
 
 });
-
-//"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg/471px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg"
